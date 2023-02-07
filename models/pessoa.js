abc499455aceba89c1eefed710bb2377a3e9fefc
GustavoBozo <87675698+GustavoBozo@users.noreply.github.com>
2023-02-07 20:20:39 -0300

    1 file modified

--- models/pessoa.js
+++ models/pessoa.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const pessoaSchema = new mongoose.Schema({
   nome: String,
   CPF: String,
-})
+});
 
-const Pessoa =  mongoose.Model('Pessoa', pessoaSchema);
+const Pessoa = mongoose.Model('Pessoa', pessoaSchema);
 
 module.exports = Pessoa;
-
